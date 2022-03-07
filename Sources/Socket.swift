61d79675eec403140abb8517bf066c8144af78b5
Dr. Michael Lauer <mickey@vanille-media.de>
2022-03-07 17:00:50 +0100

    Socket: add static method to gather available interfaces and their respective IP addresses.

--- Sources/Socket.swift
+++ Sources/Socket.swift
@@ -219,8 +219,35 @@ open class Socket {
         assert(!result.isEmpty)
         return result
     }
-    
-    
+
+    /// Returns the available network interfaces and their respective IP addresses
+    public static func availableInterfacesAndIpAddresses() -> [String: String] {
+
+        var addresses: [String: String] = [:]
+        var ifaddr: UnsafeMutablePointer<ifaddrs>?
+        guard getifaddrs(&ifaddr) == 0 else { return [:] }
+        defer { freeifaddrs(ifaddr) }
+        guard let firstAddr = ifaddr else { return [:] }
+
+        for ptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
+            let flags = Int32(ptr.pointee.ifa_flags)
+            let addr = ptr.pointee.ifa_addr.pointee
+
+            if (flags & (IFF_UP|IFF_RUNNING|IFF_LOOPBACK)) == (IFF_UP|IFF_RUNNING) {
+
+                if addr.sa_family == UInt8(AF_INET) || addr.sa_family == UInt8(AF_INET6) {
+                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    if (getnameinfo(ptr.pointee.ifa_addr, socklen_t(addr.sa_len), &hostname, socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST) == 0) {
+                        let name = String(cString: ptr.pointee.ifa_name)
+                        let address = String(cString: hostname)
+                        addresses[name] = address
+                    }
+                }
+            }
+        }
+        return addresses
+    }
+
     open func startTls(_ config: TLS.Configuration) throws {
         tls = try TLS(self.fileDescriptor, config)
         try tls?.handshake()
